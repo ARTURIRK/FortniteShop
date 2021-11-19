fdf0187aa216834c73a9e8a920a260276ee037ac
ARTURIRK <tsebaev.artur@mail.ru>
2021-11-19 13:07:55 +0300

    Update BasketItem.jsx

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -9,7 +9,7 @@ export function BasketItem(props){
   } = props
 
   return  <li  className="collection-item">
-      {displayName} x <i className='material-icons pros' onClick={() => increaseQuantity('minus',mainId)}>remove</i>{quantity} <i className='material-icons' onClick={() => increaseQuantity('plus',mainId)}>add</i> = {finalPrice*quantity}
+     <span> {displayName} </span>  <i className='material-icons pros' onClick={() => increaseQuantity('minus',mainId)}>remove</i>{quantity} <i className='material-icons' onClick={() => increaseQuantity('plus',mainId)}>add</i> = {finalPrice*quantity}
       <span className='secondary-content'>
       <i className='material-icons basket-delete' onClick={() => removeFromBasket(mainId)}>close</i>
     </span>
